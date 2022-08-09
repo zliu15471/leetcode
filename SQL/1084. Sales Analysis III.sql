27fc379dc8112d570bdc3f6f377b50e7093f13c2
zhenliu <zhen15471@gmail.com>
2022-08-09 16:46:47 +0100

    update

---
+++ SQL/1084. Sales Analysis III.sql
@@ -0,0 +1,47 @@
+
+Table: Product
+
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| product_id   | int     |
+| product_name | varchar |
+| unit_price   | int     |
++--------------+---------+
+product_id is the primary key of this table.
+Each row of this table indicates the name and the price of each product.
+Table: Sales
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| seller_id   | int     |
+| product_id  | int     |
+| buyer_id    | int     |
+| sale_date   | date    |
+| quantity    | int     |
+| price       | int     |
++-------------+---------+
+This table has no primary key, it can have repeated rows.
+product_id is a foreign key to the Product table.
+Each row of this table contains some information about one sale.
+ 
+
+Write an SQL query that reports the products that were only sold in the first quarter of 2019. That is, between 2019-01-01 and 2019-03-31 inclusive.
+
+Return the result table in any order.
+
+
+
+
+
+  # Write your MySQL query statement below
+SELECT  Product.product_id, Product.product_name
+FROM Sales
+lEFT JOIN Product ON Sales.product_id = Product.product_id
+WHERE Product.product_id NOT IN (
+SELECT DISTINCT Sales.product_id 
+FROM Sales
+WHERE
+Sales.sale_date NOT BETWEEN '2019-01-01' AND '2019-03-31')
+GROUP BY Product.product_id
