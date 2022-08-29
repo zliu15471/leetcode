a0b2a391f134bddcaf32475fad9668cec0ccad52
zhenliu <zhen15471@gmail.com>
2022-08-29 17:07:20 +0100

    update

---
+++ SQL/1454. Active Users.sql
@@ -0,0 +1,93 @@
+Table: Accounts
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| name          | varchar |
++---------------+---------+
+id is the primary key for this table.
+This table contains the account id and the user name of each account.
+
+
+Table: Logins
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| login_date    | date    |
++---------------+---------+
+There is no primary key for this table, it may contain duplicates.
+This table contains the account id of the user who logged in and the login date. A user may log in multiple times in the day.
+
+
+Active users are those who logged in to their accounts for five or more consecutive days.
+
+Write an SQL query to find the id and the name of active users.
+
+Return the result table ordered by id.
+
+The query result format is in the following example.
+
+
+
+Example 1:
+
+Input:
+Accounts table:
++----+----------+
+| id | name     |
++----+----------+
+| 1  | Winston  |
+| 7  | Jonathan |
++----+----------+
+Logins table:
++----+------------+
+| id | login_date |
++----+------------+
+| 7  | 2020-05-30 |
+| 1  | 2020-05-30 |
+| 7  | 2020-05-31 |
+| 7  | 2020-06-01 |
+| 7  | 2020-06-02 |
+| 7  | 2020-06-02 |
+| 7  | 2020-06-03 |
+| 1  | 2020-06-07 |
+| 7  | 2020-06-10 |
++----+------------+
+Output:
++----+----------+
+| id | name     |
++----+----------+
+| 7  | Jonathan |
++----+----------+
+Explanation:
+User Winston with id = 1 logged in 2 times only in 2 different days, so, Winston is not an active user.
+User Jonathan with id = 7 logged in 7 times in 6 different days, five of them were consecutive days, so, Jonathan is an active user.
+
+
+
+
+
+
+
+
+
+
+
+
+# Write your MySQL query statement below
+WITH cte AS(
+SELECT Logins.id, name,login_date,
+LEAD(login_date,4) OVER(PARTITION BY Logins.id ORDER BY login_date) AS rk
+FROM Logins
+JOIN Accounts ON
+Logins.id = Accounts.id
+GROUP BY Logins.id, login_date)
+
+
+SELECT DISTINCT id, name
+FROM cte
+WHERE DATEDIFF(rk,login_date) =4
+ORDER BY id
